e620201cc51e48455f0c9557cc859e316a502100
Homa_sol <homa.soleymaniha@yahoo.com>
2021-11-24 22:48:19 -0500

    fixing issue in top cards

--- Public/assets/main.js
+++ Public/assets/main.js
@@ -1,28 +1,28 @@
 var ctx = document.getElementById("myChart").getContext('2d');
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
-          datasets: [{
-            label: 'Today',// 'Yesterday',
-            data: [10, 11, 19, 20, 21, 15, 37, 40, 38, 15, 10, 20, 35, 40, 50, 25, 20, 35, 40, 60],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#007bff',
-            borderWidth: 4,
-            pointBackgroundColor: '#007bff'
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    datasets: [{
+      label: 'Today',// 'Yesterday',
+      data: [10, 11, 19, 20, 21, 15, 37, 40, 38, 15, 10, 20, 35, 40, 50, 25, 20, 35, 40, 60],
+      lineTension: 0,
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
         }
-      });
+      }]
+    },
+    legend: {
+      display: false,
+    }
+  }
+});
